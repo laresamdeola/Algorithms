91842e6afedb4002182c99a083197b59e4668856
lare <74225315+laresamdeola@users.noreply.github.com>
2022-03-27 22:52:55 +0000

    ...

--- not_very_secure.py
+++ not_very_secure.py
@@ -6,7 +6,7 @@ The string has the following conditions to be alphanumeric:
 
 At least one character ("" is not valid)
 Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
-No whitespaces / underscore
+No whitespaces or underscore
 """
 
 
