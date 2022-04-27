67fbb3f84eaf43d1aafe5c91ba82ffaea0e4e684
lare <74225315+laresamdeola@users.noreply.github.com>
2022-04-27 01:51:38 +0000

    grade book algo

---
+++ grade_book.js
@@ -0,0 +1,28 @@
+/*
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+
+function getGrade (s1, s2, s3) {
+  let average = (s1+s2+s3)/3;
+  let grade = ""
+  if(average <= 100 && average >= 90){
+    grade = "A";
+  } else if(average >= 80 && average < 90){
+    grade = "B"
+  } else if(average >= 70 && average < 80){
+    grade = "C";
+  } else if(average >= 60 && average < 70){
+    grade = "D";
+  } else{
+    grade = "F";
+  }
+  return grade;
+}
